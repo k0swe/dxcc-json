e8c9b3ad63f40f33d1bc3ec971eb9efc07fd5089
Chris Keller <xylo04@gmail.com>
2024-11-03 09:15:03 -0700

    Add regex verification to json transform script (#9)
    * Add regex verification to json transform script
    
    * Fix West Germany regex

--- dxcc.py
+++ dxcc.py
@@ -1,5 +1,7 @@
 import csv
 import json
+import sys
+import re
 
 
 def getEntityCode(e):
@@ -30,6 +32,14 @@ with open('dxcc-2020-02.csv', mode='r', encoding='utf8') as infile:
         row['cq'] = []
         for c in temp:
             row['cq'].append(int(c.strip()))
+
+        rgex = row['prefixRegex']
+        temp = row['prefix'].split(',')
+        for p in temp:
+            if not re.search(rgex, p):
+                print('Warning: regex for "'+row['name']+'" doesn\'t match prefix:', p, file=sys.stderr)
+        
+        # TODO: test against all other regexes to test for mutual exclusivity
 
         dxccList.append(row)
 
